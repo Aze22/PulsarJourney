ddb32af2a972af9416d564f828cd518981ea8788
Charles Pearson <charles.bernardoff@gmail.com>
2016-09-18 19:30:02 +0200

    Fixes for previous, now skipping directly to quotes

--- PulsarJourney/Assets/GalleryController.cs
+++ PulsarJourney/Assets/GalleryController.cs
@@ -75,13 +75,13 @@ public class GalleryController : MonoBehaviour {
         m_chapterText.text = Localization.Get(string.Concat("Chapter_", m_currentChapter, "_Title"));
         m_contentText.text = Localization.Get(string.Concat("Chapter_", m_currentChapter, "_Content"));
 
-        CancelInvoke("NextClicked");
+        CancelInvoke("AutoNextClicked");
 
         if ( m_currentChapter == 1 || m_currentChapter == 13 )
         {
             m_centerText.text = m_contentText.text;
             SetQuoteMode(true);
-            Invoke("NextClicked", 4);
+            Invoke("AutoNextClicked", 4);
         }
         else
         {
@@ -92,9 +92,18 @@ public class GalleryController : MonoBehaviour {
             m_chapterText.text = "";
     }
 
+    public void AutoNextClicked()
+    {
+        m_nextButton.interactable = false;
+        m_previousButton.interactable = false;
+        NextClicked();
+    }
+
     public void NextClicked()
     {
-        if(m_currentChapter < 14)
+        CancelInvoke("AutoNextClicked");
+
+        if (m_currentChapter < 14)
             ChangeToChapter(m_currentChapter + 1);
 
         //CheckButtons();
@@ -128,7 +137,9 @@ public class GalleryController : MonoBehaviour {
         yield return new WaitForSeconds(1f);
         SetContentToChapter(_newChapter);
         yield return null;
-       // m_scrollRect.verticalScrollbar.value = 1f;
+        m_nextButton.interactable = true;
+        m_previousButton.interactable = true;
+        // m_scrollRect.verticalScrollbar.value = 1f;
         m_scrollRect.verticalNormalizedPosition = 1;
         CheckButtons();
         TweenGroupAlpha.Begin(m_contentCanvas, 0.5f, 1);
@@ -136,8 +147,17 @@ public class GalleryController : MonoBehaviour {
 
     public void PreviousClicked()
     {
-        if(m_currentChapter > 0)
-            ChangeToChapter(m_currentChapter - 1);
+        CancelInvoke("AutoNextClicked");
+
+        if ( m_currentChapter > 0 )
+        {
+            if ( m_currentChapter == 2  || m_currentChapter == 3 || m_currentChapter == 14 )
+            {
+                ChangeToChapter(m_currentChapter - 2);
+            }
+            else
+                ChangeToChapter(m_currentChapter - 1);
+        }
 
         //CheckButtons();
     }
